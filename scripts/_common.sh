0d4e46a60b75f6195cb6c1794f242c1b83c12836
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-15 12:19:13 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="ca-certificates curl rsync"
+#REMOVEME? pkg_dependencies="ca-certificates curl rsync"
 
 NODEJS_VERSION=10
 
